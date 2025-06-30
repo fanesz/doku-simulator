b9304f2ab08201e4edff622465e99081fae3f3d8
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-06-30 16:16:24 +0700

    remove: unused warp token

--- src/api/services/permata/service.ts
+++ src/api/services/permata/service.ts
@@ -5,10 +5,6 @@ export default class PermataService {
   private api: API = new API();
 
   async inquiry(endpoint: string, data: PermataInquiryRequest) {
-    this.api.setHeaders({
-      "x-warp-api-key": "Jsm7Ih0yY0MUbZw2uugr",
-      "x-warp-session-token": "172qyo-ihcx4tmbubgclj14ztanxf",
-    })
     const res = await this.api.POST(endpoint.slice(0, -2) + "/inquiry", data);
     return res;
   }
